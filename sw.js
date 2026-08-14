7426555a977c464c9e8ed3a00146f45373f8d718
Habibur-365 <habibur.hrm365@gmail.com>
2026-08-14 18:02:15 +0600

    Fixed mobile bottom nav and viewport issues

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 // Fund Manager Service Worker v1.0
 // Enables offline functionality and PWA install on mobile
 
-const CACHE_NAME = 'hr-fund-manager-v4';
+const CACHE_NAME = 'hr-fund-manager-v5';
 const STATIC_ASSETS = [
   './',
   './index.html',
