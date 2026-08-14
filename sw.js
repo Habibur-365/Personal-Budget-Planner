fbe318fff65c004d41434b06c185fd6bfb3ee203
Habibur-365 <habibur.hrm365@gmail.com>
2026-08-14 18:17:25 +0600

    Fixed mobile zoom and layout scale issues

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 // Fund Manager Service Worker v1.0
 // Enables offline functionality and PWA install on mobile
 
-const CACHE_NAME = 'hr-fund-manager-v5';
+const CACHE_NAME = 'hr-fund-manager-v6';
 const STATIC_ASSETS = [
   './',
   './index.html',
