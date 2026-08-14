123d5dbdaff2204a51b4418678a45ffd7b62c8c4
Habibur-365 <habibur.hrm365@gmail.com>
2026-08-14 23:59:42 +0600

    Fixed page routing flicker on reload (Anti-FOUC script)

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 // Fund Manager Service Worker v1.0
 // Enables offline functionality and PWA install on mobile
 
-const CACHE_NAME = 'hr-fund-manager-v17';
+const CACHE_NAME = 'hr-fund-manager-v18';
 const STATIC_ASSETS = [
   './',
   './index.html',
