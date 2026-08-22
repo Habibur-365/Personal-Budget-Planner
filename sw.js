a75bf0e464f591a75771c6ae88a13b164b8a0592
Habibur-365 <habibur.hrm365@gmail.com>
2026-08-22 23:15:27 +0600

    Update SW cache version to force client update

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 // Fund Manager Service Worker v1.0
 // Enables offline functionality and PWA install on mobile
 
-const CACHE_NAME = 'hr-fund-manager-v19';
+const CACHE_NAME = 'hr-fund-manager-v20';
 const STATIC_ASSETS = [
   './',
   './index.html',
